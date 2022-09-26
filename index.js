1db1c92fb4adf63c7bc695ef460b56b611b8d8ea
Dave Greber <davegreber@gmail.com>
2022-09-26 09:58:14 -0400

    commit tree 1f24cb050a33099e9f90bc505194298cd38f35b5

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  // .then(function (response) {
+  //   console.log(response);
+  //   return response.json();
+  // })
+  // .then(function(json) {
+  //   console.log(json);
+  //   return json;
+  // });
+
+  .then(response => response.json())
+  .then(json => renderBooks(json))
+
 }
 
 function renderBooks(books) {
@@ -9,9 +21,13 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
